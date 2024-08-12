03cb9da6eb1abf8131eb166bc4d1beec9f1cd65f
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-12 07:12:36 +0600

    case-sensitive

---
+++ case-convert.js
@@ -0,0 +1,12 @@
+const anthem = 'Amar Sonar Bangla Ami Tomai Valobashi';
+const search = 'valo';
+
+
+
+const userInput = 'blaCKPink';
+const savedUsername = 'blackPink';
+// console.log(userInput.toLocaleLowerCase());
+// console.log(savedUsername.toLocaleLowerCase())
+if(userInput.toLocaleLowerCase() == savedUsername.toLocaleLowerCase()){
+    console.log('user exists');
+}
