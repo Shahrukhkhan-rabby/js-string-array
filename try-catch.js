2b4c1d7a212fe497dc85994dd12682ef42fef633
Md. Shahrukh Khan <shahrukhkhan3418198@gmail.com>
2024-08-12 19:52:46 +0600

    try-catch-error

---
+++ try-catch.js
@@ -0,0 +1,22 @@
+const myName = 'ami';
+const student = undefined;
+
+// console.log(student());
+
+try{
+    myName = 'tumi';
+}
+catch(er){
+    console.log('getting error', error);
+
+}
+finally{
+    
+}
+console.log(100);
+console.log(myName);
+// for(let i = 0; i<10 i++){
+
+// }
+// name = 'tumi';
+// console.log(name);
